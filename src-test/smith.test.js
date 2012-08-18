a1ca386779dab7cf6b07493a17b6bf8d0afce327
Todd Wolfson <todd@twolfson.com>
2012-08-18 15:45:53 -0700

    Canvas is working except for this drawImage part

--- src-test/smith.test.js
+++ src-test/smith.test.js
@@ -1,2 +1,11 @@
-var smith = require('../src/smith.js');
-smith([__dirname + '/test_sprites/sprite1.png'], console.log);
+var smith = require('../src/smith.js'),
+    sprites = [__dirname + '/test_sprites/sprite1.png'];
+
+// Attempt to smith out the sprites
+smith(sprites, function (err, result) {
+  if (err) {
+    throw err;
+  } else {
+    console.log('RESULT: ', result);
+  }
+});
--- src/smith.js
+++ src/smith.js
@@ -1,14 +1,45 @@
 var async = require('async'),
-    fs = require('fs');
+    fs = require('fs'),
+    Canvas = require('canvas'),
+    Image = Canvas.Image;
 
 // Generate the spritesmith function
 function Spritesmith(files, callback) {
-  async.map(files, function (file, cb) {
-    fs.readFile(file, 'utf8', cb);
-  }, function (err, files) {
-    console.log(files);
-    callback('2');
-  });
+  // In a waterfall fashion
+  async.waterfall([
+    // Retrieve the files
+    function smithRetrieveFiles (cb) {
+      async.map(files, function (file, cb) {
+        fs.readFile(file, 'utf8', cb);
+      }, cb);
+    },
+    // Then, create a canvas and the files to it
+    function smithAddFiles (files, cb) {
+      // TODO: Predict the optimum size canvas
+      // Create a canvas
+      var canvas = new Canvas(100, 100),
+          ctx = canvas.getContext('2d');
+
+      // Add the files to the canvas
+      // TODO: Use a better algorithm
+      // TODO: Should this be async.each?
+      files.forEach(function (file) {
+        // Create an Image from the file
+        var img = new Image();
+        img.src = file;
+        // ctx.drawImage(img, 0, 0, img.width, img.height);
+      });
+// console.log(canvas.toDataUrl(function (err, data) {
+// console.log(arguments);
+// }));
+      // Callback with the canvas
+      cb(null, canvas);
+    },
+    // Then, callback with the output canvas
+    function tempCallback() {
+      arguments[arguments.length - 1](null, '2');
+    }
+  ], callback);
 }
 
 // Export Spritesmith
