fb1cb1211d3e312dbba286582e9cc7dcd36958db
Todd Wolfson <todd@twolfson.com>
2012-08-18 17:40:49 -0700

    Everything working in full. Incrementing version number

--- src-test/smith.test.js
+++ src-test/smith.test.js
@@ -11,14 +11,33 @@ smith(sprites, function (err, result) {
   if (err) {
     throw err;
   } else {
+    var expectedDir = __dirname + '/expected_files';
     // DEV: Write out the result to a file
     // fs.writeFileSync(__dirname + '/test_sprites/expected.png', result.image, 'binary');
 
     // Assert the actual image is the same expected
-    var expectedFile = spriteDir + '/expected.png',
+    var expectedFile = expectedDir + '/sprite.png',
         expectedImage = fs.readFileSync(expectedFile, 'binary');
     assert.strictEqual(expectedImage, result.image, "Actual image does not match expected image");
-console.log(result.coordinates);
+
+    // Load in the coordinates
+    var expectedCoords = require(expectedDir + '/coordinates.json');
+
+    // Normalize the actual coordinates
+    // TODO: Normalize dir should be an option
+    var actualCoords = result.coordinates,
+        normCoords = {},
+        path = require('path');
+    assert(actualCoords, "Result does not have a coordinates property");
+
+    Object.getOwnPropertyNames(actualCoords).forEach(function (filepath) {
+      var file = path.relative(spriteDir, filepath);
+      normCoords[file] = actualCoords[filepath];
+    });
+
+    // Assert that the returned coordinates deep equal those in the coordinates.json
+    assert.deepEqual(expectedCoords, normCoords, "Actual coordinates do not match expected coordinates");
+
     // Notify that the test is passing
     console.log("Success!");
   }
