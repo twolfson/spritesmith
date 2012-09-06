97b9319056bb1b5ead622c21f0d62e509150a79b
Todd Wolfson <todd.wolfson@ensighten.com>
2012-09-06 09:58:21 -0700

    Working but page isn't functioning as it used to

--- src/engines/gm.js
+++ src/engines/gm.js
@@ -6,48 +6,50 @@ var fs = require('fs'),
     exporters = {
       'png': function canvasPngExporter (cb) {
         var canvas = this.canvas;
-        canvas.stream(function (err, stdout, stderr) {
-          // If there was an error, callback with it
-          if (err) {
-            return cb(err);
-          }
+canvas.write('omnom.png', function () {console.log(arguments);});
+        // canvas.stream(function (err, stdout, stderr) {
+          // // If there was an error, callback with it
+          // if (err) {
+            // return cb(err);
+          // }
 
-          // Otherwise, create placeholder items
-          var imgData = [],
-              err;
+          // // Otherwise, create placeholder items
+          // var imgData = [],
+              // err;
 
-          // On data, add it to imgData
-          // Note: We must save in 'binary' since utf8 strings don't support any possible character that a file might use
-          stdout.on('data', function (chunk) {
-            var binaryStr = chunk.toString('binary');
-            imgData.push(binaryStr);
-          });
+          // // On data, add it to imgData
+          // // Note: We must save in 'binary' since utf8 strings don't support any possible character that a file might use
+          // stdout.on('data', function (chunk) {
+            // var binaryStr = chunk.toString('binary');
+            // imgData.push(binaryStr);
+          // });
 
-          // On error, save it
-          stderr.on('data', function (_err) {
-            err = _err;
-          });
+          // // On error, save it
+          // stderr.on('data', function (_err) {
+            // err = _err;
+          // });
 
-          // When complete
-          stdout.on('end', function () {
-            // If there was an error, callback with it
-            if (err) {
-              cb(err);
-            } else {
-            // Otherwise, join together image data, put it into the retObj
-              var retStr = imgData.join('');
+          // // When complete
+          // stdout.on('end', function () {
+            // // If there was an error, callback with it
+            // if (err) {
+              // cb(err);
+            // } else {
+            // // Otherwise, join together image data, put it into the retObj
+              // var retStr = imgData.join('');
 
-              // Callback with no error
-              cb(null, retStr);
-            }
-          });
-        });
+              // // Callback with no error
+              // cb(null, retStr);
+            // }
+          // });
+        // });
       }
     },
     engine = {};
 
 function Canvas(file) {
-  var canvas = gm(file);
+  // var canvas = gm(file);
+  var canvas = gm(100, 300, 'transparent');
   this.canvas = canvas;
 }
 Canvas.prototype = {
@@ -57,7 +59,8 @@ Canvas.prototype = {
 
     // TODO: Pull request this in
     canvas.out('-page');
-    canvas.out('+' + x + '+' + y);
+// console.log(img.width, img.height, x, y);
+    canvas.out(img.width + 'x' + img.height + '+' + x + '+' + y);
     canvas.out(img.file);
   },
   'export': function exportFn (format, cb) {
@@ -72,23 +75,25 @@ Canvas.prototype = {
 
     async.waterfall([
       function outputImage (cb) {
-        // Flatten the image
-        canvas.flatten();
+        // Flatten the image (with transparency)
+        canvas.mosaic();
+        // canvas.flatten();
+console.log(canvas);
 
         // Render the item
         exporter.call(that, cb);
       },
       function cleanupTmpfile (output, cb) {
-        // Attempt to delete the tmpfile
-        fs.unlink(tmpfile, function (err) {
-          // If there is an error, log it
-          if (err) {
-            console.error('Warning: Could not delete temporary spritesmith file!' + tmpfile, err);
-          }
+        // // Attempt to delete the tmpfile
+        // fs.unlink(tmpfile, function (err) {
+          // // If there is an error, log it
+          // if (err) {
+            // console.error('Warning: Could not delete temporary spritesmith file!' + tmpfile, err);
+          // }
 
           // Continue with the output
           cb(null, output);
-        });
+        // });
       }
     ], cb);
   }
@@ -97,6 +102,7 @@ Canvas.prototype = {
 // Expose Canvas to engine
 engine.Canvas = Canvas;
 
+// TODO: Make this a spritesmith helper
 // Create paths for the scratch directory and transparent pixel
 var scratchDir = path.join(__dirname, '../../scratch'),
     transparentPixel = path.join(__dirname, 'transparent.png');
@@ -104,7 +110,8 @@ function createCanvas(width, height, cb) {
   var now = +new Date(),
       rand = Math.random(),
       randomFilename = now + '.' + rand + '.png',
-      tmpfile = path.join(scratchDir, randomFilename);
+      // tmpfile = path.join(scratchDir, randomFilename);
+      tmpfile = path.join(scratchDir, 'test.png');
   async.waterfall([
     function guaranteeScratchDir (cb) {
       // This always passes due to error annoyances
@@ -112,15 +119,16 @@ function createCanvas(width, height, cb) {
         cb(null);
       });
     },
-    function generateCanvas (cb) {
-      // TODO: Get it working without transparent.png (i.e. use the next line)
-      // var canvas = gm(525, 110, "#00ff55aa");
-      var base = gm(transparentPixel).extent(width, height);
+    // function generateCanvas (cb) {
+      // // TODO: Get it working without transparent.png (i.e. use the next line)
+      // // var canvas = gm(525, 110, "#00ff55aa");
+      // var base = gm(transparentPixel).extent(width, height);
 
-      // Write out the base file
-      base.write(tmpfile, cb);
-    },
-    function loadBackCanvas (x, y, z, cb) {
+      // // Write out the base file
+      // base.write(tmpfile, cb);
+    // },
+    // function loadBackCanvas (x, y, z, cb) {
+    function loadBackCanvas (cb) {
       // Create a canvas
       var canvas = new Canvas(tmpfile);
 
