a94793790a3b0cfbff31e0d8892557897c5c7501
Todd Wolfson <todd.wolfson@ensighten.com>
2012-09-14 00:42:39 -0700

    After a long break, finally have transparent gm working. Now for cleanup.

--- src/engines/gm.js
+++ src/engines/gm.js
@@ -7,52 +7,53 @@ var fs = require('fs'),
       'png': function canvasPngExporter (cb) {
         var canvas = this.canvas;
 canvas.write('omnom.png', function () {console.log(arguments);});
-        // canvas.stream(function (err, stdout, stderr) {
-          // // If there was an error, callback with it
-          // if (err) {
-            // return cb(err);
-          // }
+        canvas.stream(function (err, stdout, stderr) {
+          // If there was an error, callback with it
+          if (err) {
+            return cb(err);
+          }
 
-          // // Otherwise, create placeholder items
-          // var imgData = [],
-              // err;
+          // Otherwise, create placeholder items
+          var imgData = [],
+              err;
 
-          // // On data, add it to imgData
-          // // Note: We must save in 'binary' since utf8 strings don't support any possible character that a file might use
-          // stdout.on('data', function (chunk) {
-            // var binaryStr = chunk.toString('binary');
-            // imgData.push(binaryStr);
-          // });
+          // On data, add it to imgData
+          // Note: We must save in 'binary' since utf8 strings don't support any possible character that a file might use
+          stdout.on('data', function (chunk) {
+            var binaryStr = chunk.toString('binary');
+            imgData.push(binaryStr);
+          });
 
-          // // On error, save it
-          // stderr.on('data', function (_err) {
-            // err = _err;
-          // });
+          // On error, save it
+          stderr.on('data', function (_err) {
+            err = _err;
+          });
 
-          // // When complete
-          // stdout.on('end', function () {
-            // // If there was an error, callback with it
-            // if (err) {
-              // cb(err);
-            // } else {
-            // // Otherwise, join together image data, put it into the retObj
-              // var retStr = imgData.join('');
+          // When complete
+          stdout.on('end', function () {
+            // If there was an error, callback with it
+            if (err) {
+              cb(err);
+            } else {
+            // Otherwise, join together image data, put it into the retObj
+              var retStr = imgData.join('');
 
-              // // Callback with no error
-              // cb(null, retStr);
-            // }
-          // });
-        // });
+              // Callback with no error
+              cb(null, retStr);
+            }
+          });
+        });
       }
     },
     engine = {};
 
 function Canvas(file) {
-  var canvas = gm(file);
-  canvas.out('-background');
-  canvas.out('transparent');
-  canvas.out('xc:transparent');
-  // var canvas = gm(100, 300, 'transparent');
+  var canvas = gm(1, 1, 'transparent');
+  
+  // Override the -size options (won't work otherwise)
+  canvas._in = ['-background', 'transparent'];
+  
+  // Save the canvas
   this.canvas = canvas;
 }
 Canvas.prototype = {
@@ -62,8 +63,7 @@ Canvas.prototype = {
 
     // TODO: Pull request this in
     canvas.out('-page');
-// console.log(img.width, img.height, x, y);
-    canvas.out(img.width + 'x' + img.height + '+' + x + '+' + y);
+    canvas.out('+' + x + '+' + y);
     canvas.out(img.file);
   },
   'export': function exportFn (format, cb) {
@@ -80,8 +80,6 @@ Canvas.prototype = {
       function outputImage (cb) {
         // Flatten the image (with transparency)
         canvas.mosaic();
-        // canvas.flatten();
-console.log(canvas);
 
         // Render the item
         exporter.call(that, cb);
@@ -113,8 +111,8 @@ function createCanvas(width, height, cb) {
   var now = +new Date(),
       rand = Math.random(),
       randomFilename = now + '.' + rand + '.png',
-      // tmpfile = path.join(scratchDir, randomFilename);
-      tmpfile = path.join(scratchDir, 'test.png');
+      tmpfile = path.join(scratchDir, randomFilename);
+      // tmpfile = path.join(scratchDir, 'test.png');
   async.waterfall([
     function guaranteeScratchDir (cb) {
       // This always passes due to error annoyances
@@ -122,16 +120,16 @@ function createCanvas(width, height, cb) {
         cb(null);
       });
     },
-    // function generateCanvas (cb) {
-      // // TODO: Get it working without transparent.png (i.e. use the next line)
-      // // var canvas = gm(525, 110, "#00ff55aa");
+    function generateCanvas (cb) {
+      // TODO: Get it working without transparent.png (i.e. use the next line)
+      // var canvas = gm(525, 110, "#00ff55aa");
       // var base = gm(transparentPixel).extent(width, height);
+      var base = gm(100, 300, 'transparent');
 
-      // // Write out the base file
-      // base.write(tmpfile, cb);
-    // },
-    // function loadBackCanvas (x, y, z, cb) {
-    function loadBackCanvas (cb) {
+      // Write out the base file
+      base.write(tmpfile, cb);
+    },
+    function loadBackCanvas (x, y, z, cb) {
       // Create a canvas
       var canvas = new Canvas(tmpfile);
 
