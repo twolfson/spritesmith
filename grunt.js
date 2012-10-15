d906922fe80325a0bd04d1b5dcafda21f147df6f
Todd Wolfson <todd@twolfson.com>
2012-10-15 00:04:27 -0700

    Added grunt.js and .npmignore

---
+++ grunt.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    lint: {
+      files: ['grunt.js', 'src/**/*.js', 'src-test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true
+      },
+      globals: {
+        exports: true
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+};
