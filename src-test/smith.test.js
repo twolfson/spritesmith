ebb86a5b575661cc32067ee9c446406b8037b3ef
Todd Wolfson <todd@twolfson.com>
2012-11-16 22:56:00 -0800

    Namespacing inside of tests finalized

--- src-test/smith.test.js
+++ src-test/smith.test.js
@@ -16,6 +16,9 @@ describe('An array of sprites', function () {
     before(function (done) {
       var that = this;
 
+      // Create a namespace for loading from
+      this.namespace = 'topDown.';
+
       // Attempt to smith out the sprites
       // smith({'src': sprites, 'algorithm': 'right-left'}, function (err, result) {
       // smith({'src': sprites, 'engine': 'gm', 'exportOpts': {'format': 'jpg', 'quality': 20}}, function (err, result) {
@@ -34,7 +37,9 @@ describe('An array of sprites', function () {
     });
 
     it('renders a top-down spritesheet', function () {
-      var result = this.result;
+      var result = this.result,
+          namespace = this.namespace;
+
       // DEV: Write out the result to a file
       // fs.writeFileSync(expectedDir + '/gm.png', result.image, 'binary');
 
@@ -50,9 +55,9 @@ describe('An array of sprites', function () {
 
       // Assert the actual image is the same expected
       var actualImage = result.image,
-          expectedCanvasFile = path.join(expectedDir, 'canvas.png'),
-          expectedGmFile = path.join(expectedDir, 'gm.png'),
-          expectedGm2File = path.join(expectedDir, 'gm2.png'),
+          expectedCanvasFile = path.join(expectedDir, namespace + 'canvas.png'),
+          expectedGmFile = path.join(expectedDir, namespace + 'gm.png'),
+          expectedGm2File = path.join(expectedDir, namespace + 'gm2.png'),
           expectedCanvasImage = fs.readFileSync(expectedCanvasFile, 'binary'),
           expectedGmImage = fs.readFileSync(expectedGmFile, 'binary'),
           expectedGm2Image = fs.readFileSync(expectedGm2File, 'binary'),
@@ -67,7 +72,7 @@ describe('An array of sprites', function () {
     it('has the proper coordinates', function () {
       // Load in the coordinates
       var result = this.result,
-          expectedCoords = require(expectedDir + '/coordinates.json');
+          expectedCoords = require(expectedDir + '/' + this.namespace + 'coordinates.json');
 
       // Normalize the actual coordinates
       // TODO: Normalize dir should be an option
