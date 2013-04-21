9b0a49519d2bc2327f0dd1c622de44e5747b3cc7
Todd Wolfson <todd@twolfson.com>
2013-04-20 23:23:06 -0700

    Testing all current engines =D

--- src-test/run.sh
+++ src-test/run.sh
@@ -1,3 +1,3 @@
 # TODO: Probably should move to Makefile?
 # DEV: If we move off of .sh, verify that doubleshot is on the path even if not installed
-doubleshot --outline src-test/smith.outline.js --content src-test/$1.content.js
+doubleshot --outline smith.outline.js --content $1.content.js
