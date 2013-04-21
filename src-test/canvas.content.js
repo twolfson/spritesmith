85f057927d66817e72290120846f8e96f38467cc
Todd Wolfson <todd@twolfson.com>
2013-04-20 22:59:26 -0700

    Added canvas test and found another doubleshot issue o_o

---
+++ src-test/canvas.content.js
@@ -0,0 +1,21 @@
+// Load in underscore and common test
+var _ = require('underscore'),
+    common = require('./smith.content');
+
+module.exports = _.defaults({
+  'using canvas engine': function () {
+    // Localize and fallback options
+    var options = this.options || {};
+
+    // Add `canvas` as engine
+    options.engine = 'canvas';
+
+    console.log('aaa', options.engine);
+    // Save reference
+    this.options = options;
+  },
+  'An array of sprites': ['_An array of sprites', 'using canvas engine'],
+  'when converted from left to right': ['_when converted from left to right', 'using canvas engine'],
+  'An empty array': ['_An empty array', 'using canvas engine'],
+  'A ridiculous amount of sprites': ['_A ridiculous amount of sprites', 'using canvas engine']
+}, common);
