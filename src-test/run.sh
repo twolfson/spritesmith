ad0368ac1b72f85eb54f0fcb5c38af226705c81b
Todd Wolfson <todd@twolfson.com>
2013-04-27 00:51:08 -0700

    Attempting to move to node for common script

--- src-test/run.sh
+++
@@ -1,3 +0,0 @@
-# TODO: Probably should move to Makefile?
-# DEV: If we move off of .sh, verify that doubleshot is on the path even if not installed
-doubleshot --outline smith.outline.js --content $1.content.js
