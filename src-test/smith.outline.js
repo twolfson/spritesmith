5248314818dc2d78bc00636cbd664be39ff1349a
Todd Wolfson <todd@twolfson.com>
2014-02-08 14:19:33 -0800

    Normalizing to single quotes for consistency

--- src-test/smith.outline.js
+++ src-test/smith.outline.js
@@ -1,30 +1,30 @@
 // Generate basic outlines
 var outlines = [{
-  "An array of sprites": [{
-    "when processed via spritesmith": [
-      "renders a top-down spritesheet",
-      "has the proper coordinates",
-      "has the proper properties"
+  'An array of sprites': [{
+    'when processed via spritesmith': [
+      'renders a top-down spritesheet',
+      'has the proper coordinates',
+      'has the proper properties'
     ]
   }, {
-    "when converted from left to right": [
-      "renders a left-right spritesheet",
-      "has the proper coordinates",
-      "has the proper properties"
+    'when converted from left to right': [
+      'renders a left-right spritesheet',
+      'has the proper coordinates',
+      'has the proper properties'
     ]
   }, {
-    "when provided with a padding parameter": [
-      "renders a padded spritesheet",
-      "has the proper coordinates",
-      "has the proper properties"
+    'when provided with a padding parameter': [
+      'renders a padded spritesheet',
+      'has the proper coordinates',
+      'has the proper properties'
     ]
   }]
 }, {
-  "An empty array": [{
-    "when processed via spritesmith": [
-      "renders an empty spritesheet",
-      "returns an empty coordinate mapping",
-      "has the proper properties"
+  'An empty array': [{
+    'when processed via spritesmith': [
+      'renders an empty spritesheet',
+      'returns an empty coordinate mapping',
+      'has the proper properties'
     ]
   }]
 }];
