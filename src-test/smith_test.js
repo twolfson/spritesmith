9799e482ee78c82acbd4b160933a9a74889d2f34
Todd Wolfson <todd@twolfson.com>
2014-12-14 04:19:01 -0600

    Moved new test to skip for now

--- src-test/smith_test.js
+++ src-test/smith_test.js
@@ -189,9 +189,14 @@ describe('An empty array', function () {
   });
 });
 
-describe('`spritesmith` using a custom engine via string', function () {
+describe.skip('`spritesmith` using a custom engine via string', function () {
   describe('processing a set of images', function () {
+    it('has no errors', function () {
 
+    });
+    it('renders a spritesheet', function () {
+
+    });
   });
 });
 
